bd6ba06e27eadad1d8b303c1b8d0bb2e45f7c644
CookiePenguin <zakhar.maksimkin@gmail.com>
2021-12-21 07:13:06 +0300

    Изменение 0.1 Появление заставки

--- Programs/program.py
+++ Programs/program.py
@@ -1,6 +1,75 @@
-def main():
-    print('My first git program')
+import os
+import sys
+import pygame
+
+pygame.init()
+size = width, height = 1200, 800
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption('Игра Поле чудес!')
+clock = pygame.time.Clock()
+FPS = 50
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
 
 
-if __name__ == '__main__':
-    main()
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+def start_screen():
+    intro_text = ["ЗАСТАВКА", "",
+                  "Правила игры",
+                  "Отгадывайте слова",
+                  "Получайте баллы",
+                  "Выигрывайте призы!","",
+                  "(для продолжения нажмите любую клавишу)"]
+
+
+    fon = pygame.transform.scale(load_image('fon.jpg'), (width, height))
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font(None, 30)
+    text_coord = 50
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('white'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN or \
+                    event.type == pygame.MOUSEBUTTONDOWN:
+                return  # начинаем игру
+        pygame.display.flip()
+        clock.tick(FPS)
+
+start_screen()
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill(pygame.Color("black"))
+    pygame.display.flip()
+    clock.tick(50)
+
+pygame.quit()
